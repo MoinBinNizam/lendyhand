8782e00ba9d783ad18fe595fca44be656f7c5bc7
Moin uddin <moinuddinpkt@gmail.com>
2020-10-24 21:27:05 +0600

    new file added

---
+++ contacts -copy.php
@@ -0,0 +1,376 @@
+<?php
+require('top.php');
+//require ('connection.inc.php');
+$msg="";
+if(isset($_POST['submit'])){
+    $name=mysqli_real_escape_string($con, $_POST['name']);
+    $email=mysqli_real_escape_string($con, $_POST['email']);
+    $mobile=mysqli_real_escape_string($con, $_POST['mobile']);
+    $comment=mysqli_real_escape_string($con, $_POST['comment']);
+
+    mysqli_query("insert into contact_us(name,email,mobile,comment) values ('$name','$email','$mobile','$comment')");
+    $msg="Thank you";
+
+
+}
+//default variables and set to empty values
+$name_error = $email_error = $mobile_error = $comment_error= "";
+$name= $email = $mobile =$comment = "";
+
+//form submitted with post method
+if ($_SERVER["REQUEST_METHOD"] =="POST"){
+    if(empty($_POST["name"])){
+        $name_error ="Name is required";
+    }else{
+        $name = test_input($_POST['name']);
+        //check if name only contains letters and whitespaces
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)){
+            $name_error = "Only letters and white spaces are allowed";
+        }
+    }
+    if(empty($_POST["email"])){
+        $email_error ="Email is required";
+    }else{
+        $email = test_input($_POST['email']);
+        //check if email address is well formed
+        if (!filter_var($email,FILTER_VALIDATE_EMAIL)){
+            $email_error = "Invalid email format";
+        }
+    }
+    if(empty($_POST["mobile"])){
+        $mobile_error ="Mobile number is required";
+    }else{
+        $mobile = test_input($_POST['mobile']);
+        //check if phone number is well formed
+        if (!preg_match("/^(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}$/i",$mobile)) {
+            $mobile_error = "Invalid mobile number";
+        }
+        }
+    if(empty($_POST['comment'])){
+        $comment = "Input your comment";
+    }else{
+        $comment = test_input($_POST['comment']);
+    }
+//    if($name_error =='' and $email_error =='' and  $mobile_error =='' ){
+//        $message_body = '';
+//        unset($_POST['submit']);
+//        foreach ($_POST as $key => $value){
+//            $message_body .= "$key: $value \n";
+//        }
+//    }
+//    $to = 'moinuddinpkt@gmail.com';
+//    $subject= 'Contact Form Subject';
+//    if(mail($to,$subject,$message_body)) {
+//    $success = "Message sent, thank you for contacting us!";
+//    $name = $email = $mobile = $comment ='';
+//    }
+}
+function test_input($data){
+    $data   = trim($data);
+    $data   = stripslashes($data);
+    $data   = htmlspecialchars($data);
+    return $data;
+}
+?>
+<!-- Start Bradcaump area -->
+        <div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/2.jpg) no-repeat scroll center center / cover ;">
+            <div class="ht__bradcaump__wrap">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="bradcaump__inner">
+                                <nav class="bradcaump-inner">
+                                  <a class="breadcrumb-item" href="index.html">Home</a>
+                                  <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
+                                  <span class="breadcrumb-item active">Contact Us</span>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Bradcaump area -->
+        <!-- Start Contact Area -->
+        <section class="htc__contact__area ptb--100 bg__white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 col-md-6 col-sm-12 col-xs-12">
+                        <div class="map-contacts--2">
+                            <div id="googleMap"></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12">
+                        <h2 class="title__line--6">CONTACT US</h2>
+                        <div class="address">
+                            <div class="address__icon">
+                                <i class="icon-location-pin icons"></i>
+                            </div>
+                            <div class="address__details">
+                                <h2 class="ct__title">our address</h2>
+                                <p>666 5th Ave New York, NY, United </p>
+                            </div>
+                        </div>
+                        <div class="address">
+                            <div class="address__icon">
+                                <i class="icon-phone icons"></i>
+                            </div>
+                            <div class="address__details">
+                                <h2 class="ct__title">Phone Number</h2>
+                                <p>123-6586-587456</p>
+                            </div>
+                        </div>
+                    </div>      
+                </div>
+                <div class="row">
+                    <div class="contact-form-wrap mt--60">
+                        <div class="col-xs-12">
+                            <div class="contact-title">
+                                <h2 class="title__line--6">SEND A MAIL</h2>
+                            </div>
+                        </div>
+                        <div class="col-xs-12">
+                            <form id="contact-form" action="<?php $_SERVER['PHP_SELF'];?>" method="post">
+                                <div class="single-contact-form">
+                                    <div class="contact-box name">
+                                        <input type="text" id="name" name="name" placeholder="Your Name*" tabindex="1" value="<?php echo $name ?>">
+                                        <br>
+                                        <span class="error"><?php echo $name_error; ?></span>
+                                        <input type="email" id="email" name="email" placeholder="Email*" tabindex="2" value="<?php echo $email ?>">
+                                        <span class="error"><?php echo $email_error; ?></span>
+                                        <input type="text" id="mobile" name="mobile" placeholder="Mobile*" tabindex="3" value="<?php echo $mobile?>">
+                                        <span class="error"><?php echo $mobile_error; ?></span>
+                                    </div>
+                                </div>
+                                <div class="single-contact-form">
+                                    <div class="contact-box message">
+                                        <textarea name="comment" id="comment" placeholder="Your Comment" tabindex="4" value="<?php echo $comment ?>"></textarea>
+                                        <div class="success"><?php echo $msg;?></div>
+                                    </div>
+                                </div>
+                                <div class="contact-btn">
+                                    <button type="submit" class="fv-btn" name="send_msg">Send Messege</button>
+                                <span class="success"><?php echo $msg;?></span>
+                                </div>
+                            </form>
+                            <div class="form-output">
+                                <p class="form-messege"></p>
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </section>
+        <!-- End Contact Area -->
+		<!-- Google Map js -->
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBmGmeot5jcjdaJTvfCmQPfzeoG_pABeWo "></script>
+		<script src="js/contact-map.js"></script>
+		<script>
+			// When the window has finished loading create our google map below
+        google.maps.event.addDomListener(window, 'load', init);
+
+        function init() {
+            // Basic options for a simple Google Map
+            // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+            var mapOptions = {
+                // How zoomed in you want the map to start at (always required)
+                zoom: 12,
+
+                scrollwheel: false,
+
+                // The latitude and longitude to center the map (always required)
+                center: new google.maps.LatLng(23.7286, 90.3854), // New York
+
+                // How you would like to style the map. 
+                // This is where you would paste any style found on Snazzy Maps.
+                 styles: 
+        [ {
+                "featureType": "all",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "saturation": 36
+                    },
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 40
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 16
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 20
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 17
+                    },
+                    {
+                        "weight": 1.2
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 20
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 21
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 17
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 29
+                    },
+                    {
+                        "weight": 0.2
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 18
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 16
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 19
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#141516"
+                    },
+                    {
+                        "lightness": 17
+                    }
+                ]
+            }
+        ]
+            };
+
+            // Get the HTML DOM element that will contain your map 
+            // We are using a div with id="map" seen below in the <body>
+            var mapElement = document.getElementById('googleMap');
+
+            // Create the Google Map using our element and options defined above
+            var map = new google.maps.Map(mapElement, mapOptions);
+
+            // Let's also add a marker while we're at it
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(23.7286, 90.3854),
+                map: map,
+                title: 'Ramble!',
+                icon: 'images/icons/map-2.png',
+                animation:google.maps.Animation.BOUNCE
+
+            });
+        }
+    </script>		
+<?php
+require('footer.php');
+
+?>
---
+++ send_message.php
@@ -0,0 +1,11 @@
+<?php
+//require ('connection.inc.php');
+//require ('functions.inc.php');
+//
+//$name=get_safe_value($con,$_POST['name']);
+//$email=get_safe_value($con,$_POST['email']);
+//$mobile=get_safe_value($con,$_POST['mobile']);
+//$message=get_safe_value($con,$_POST['message']);
+//$added_on=date('Y-m-d h:i:s');
+//mysqli_query($con,"insert into contact_us(name,email,mobile,massage,added_on) values ('$name','$email','$mobile','$message','$added_on')");
+//echo "Thank you";
